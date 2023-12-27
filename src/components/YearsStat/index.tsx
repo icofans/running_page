5a714bb8fc00ec807925b306992ba21250fc9948
jiaqiang <565057208@qq.com>
2023-12-27 09:50:40 +0800

    feat:自定义

--- src/components/YearsStat/index.tsx
+++ src/components/YearsStat/index.tsx
@@ -1,8 +1,9 @@
 import YearStat from '@/components/YearStat';
 import useActivities from '@/hooks/useActivities';
 import { INFO_MESSAGE } from '@/utils/const';
+import React from "react";
 
-const YearsStat = ({ year, onClick }: { year: string, onClick: (_year: string) => void }) => {
+const YearsStat = ({ year, onClick }) => {
   const { years } = useActivities();
   // make sure the year click on front
   let yearsArrayUpdate = years.slice();
@@ -14,23 +15,45 @@ const YearsStat = ({ year, onClick }: { year: string, onClick: (_year: string) =
   return (
     <div className="fl w-100-l pb5 pr5-l">
       <section className="pb4" style={{ paddingBottom: '0rem' }}>
+        <p style={{ lineHeight: 1.8}}>
+          强风迎面吹护拂，<br/>
+          逆风奔跑前进，<br/>
+          坚信答案就在前方，<br/>
+          何为强大，<br/>
+          何为奔跑，<br/>
+          何为生存，<br/>
+          一切都在风之彼方，<br/>
+          希望就在我们各自的心中，<br/>
+          看啊，<br/>
+          道路就在我们的脚下，<br/>
+          所以，<br/>
+          我们要奔跑于今天，<br/>
+          一直奔跑，<br/>
+          直到永恒。<br/>
+        </p>
+        <p style={quoteStyle}>&ndash;&ndash;《强风吹拂》</p>
         <p style={{ lineHeight: 1.8 }}>
-          {INFO_MESSAGE(years.length, year)}
           <br />
+          <br />
+          {INFO_MESSAGE(years.length, year)}
         </p>
       </section>
       <hr color="red" />
       {yearsArrayUpdate.map((year) => (
         <YearStat key={year} year={year} onClick={onClick} />
       ))}
-      {// eslint-disable-next-line no-prototype-builtins
-        yearsArrayUpdate.hasOwnProperty('Total') ? (
-          <YearStat key="Total" year="Total" onClick={onClick} />
-        ) : (
-          <div />
-        )}
+      {yearsArrayUpdate.hasOwnProperty('Total') ? (
+        <YearStat key="Total" year="Total" onClick={onClick} />
+      ) : (
+        <div />
+      )}
     </div>
   );
+};
+
+const quoteStyle = {
+  fontWeight:"bold",
+  textAlign: "right"
 };
 
 export default YearsStat;
--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -17,11 +17,7 @@ const data: ISiteMetadataResult = {
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://nobady.cn',
     },
   ],
 };
